1c05a0ddf6f36a79d9e71ef07ba629dd68862a5b
Amr <amruser97@gmail.com>
2023-07-06 15:28:14 +0300

    added gallery & footer

--- src/HomeComponents/Footer/Footer.js
+++ src/HomeComponents/Footer/Footer.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <ul className="footer__list">
+        <li className="item">find your dream home</li>
+        <li className="item">request proposal</li>
+        <li className="item">download home planner</li>
+        <li className="item">contact us</li>
+        <li className="item">submit your property</li>
+        <li className="item">come work with us!</li>
+      </ul>
+      <p className="footer__text">
+        &copy; Copyright 2023 Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Qui quae minima, quo sed, placeat expedita libero,
+        facilis minus eveniet mollitia corrupti. At unde accusamus nemo
+        explicabo maxime expedita ipsam iste.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/HomeComponents/Gallery/Gallery.js
+++ src/HomeComponents/Gallery/Gallery.js
@@ -2,7 +2,117 @@ import React from "react";
 import "./Gallery.scss";
 
 const Gallery = () => {
-  return <section className="gallery">Gallery</section>;
+  return (
+    <section className="gallery">
+      <figure className="gallery__item gallery__item--1">
+        <img
+          src="/images/gal-1.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--1"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--2">
+        {" "}
+        <img
+          src="/images/gal-2.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--2"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--3">
+        {" "}
+        <img
+          src="/images/gal-3.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--3"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--4">
+        {" "}
+        <img
+          src="/images/gal-4.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--4"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--5">
+        <img
+          src="/images/gal-5.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--5"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--6">
+        <img
+          src="/images/gal-6.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--6"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--7">
+        {" "}
+        <img
+          src="/images/gal-7.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--7"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--8">
+        <img
+          src="/images/gal-8.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--8"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--9">
+        {" "}
+        <img
+          src="/images/gal-9.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--9"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--10">
+        {" "}
+        <img
+          src="/images/gal-10.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--10"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--11">
+        <img
+          src="/images/gal-11.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--11"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--12">
+        {" "}
+        <img
+          src="/images/gal-12.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--12"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--13">
+        {" "}
+        <img
+          src="/images/gal-13.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--13"
+        />
+      </figure>
+      <figure className="gallery__item gallery__item--14">
+        {" "}
+        <img
+          src="/images/gal-14.jpeg"
+          alt="Gallery image"
+          className="gallery__image gallery__image--14"
+        />
+      </figure>
+    </section>
+  );
 };
 
 export default Gallery;
